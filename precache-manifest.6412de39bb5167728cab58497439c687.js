be5d3e8e6ed71b61e17944b040afdccc324c41db
trox115 <tofernandes67@gmail.com>
2020-04-14 22:28:45 +0100

    Updates

---
+++ precache-manifest.6412de39bb5167728cab58497439c687.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10876abcb5f258d3fc209d558476ad4c",
+    "url": "/mcruz/index.html"
+  },
+  {
+    "revision": "654b9fd389eabf43b0d5",
+    "url": "/mcruz/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bf04c262f09857475d88",
+    "url": "/mcruz/static/css/main.e9d647ac.chunk.css"
+  },
+  {
+    "revision": "654b9fd389eabf43b0d5",
+    "url": "/mcruz/static/js/2.ccd1cfad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mcruz/static/js/2.ccd1cfad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf04c262f09857475d88",
+    "url": "/mcruz/static/js/main.a6480a3d.chunk.js"
+  },
+  {
+    "revision": "86566c7afb73763e11a7",
+    "url": "/mcruz/static/js/runtime-main.e6c8e37d.js"
+  },
+  {
+    "revision": "1c6e2dcc60e1db34cfe4a637401d6b0f",
+    "url": "/mcruz/static/media/fotoantiga.1c6e2dcc.jpg"
+  },
+  {
+    "revision": "bc70396d3535c52fa22d1035351faad3",
+    "url": "/mcruz/static/media/instagram.bc70396d.png"
+  },
+  {
+    "revision": "074c42e2ed6848b3333acc75360af32d",
+    "url": "/mcruz/static/media/logo.074c42e2.png"
+  }
+]);
